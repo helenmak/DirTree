815b257278c3d8b4452c2e7ed90b20ce9a200df6
helenmak <elenamak@ukr.net>
2019-09-10 23:45:36 +0300

    fix sorter

--- src/utils/nodesSorter.js
+++ src/utils/nodesSorter.js
@@ -1,4 +1,5 @@
 export default function nodesSorter(prevNode, nextNode) {
+    console.log('sorter')
     if(typeof prevNode === 'object' && typeof nextNode === 'object') {
         const prevName = Object.keys(prevNode)[0].toLowerCase()
         const nextName = Object.keys(nextNode)[0].toLowerCase()
@@ -9,7 +10,10 @@ export default function nodesSorter(prevNode, nextNode) {
         return 0
     }
     
-    if(typeof prevNode === 'object') return -1
+    if(typeof prevNode === 'object') {
+        console.log(prevNode)
+        return -1
+    }
     if(typeof nextNode === 'object') return 1
     
     if(prevNode.toLowerCase() < nextNode.toLowerCase()) return -1
--- src/utils/sortDirStructure.js
+++ src/utils/sortDirStructure.js
@@ -2,14 +2,18 @@ export default function sortDirStructure(dirStructure, sorter) {
     if (!dirStructure) return null
     
     if (Array.isArray(dirStructure)) {
-        return dirStructure.sort(sorter)
+        return dirStructure.map(branch => {
+            return typeof branch === 'object'
+                   ? sortDirStructure(branch, sorter)
+                   : branch
+        }).sort(sorter)
     } else {
         const sortedDirStructure = {}
         
         for(const property in dirStructure) {
             if (!dirStructure.hasOwnProperty(property)) continue
             
-            const sortedVal = sortDirStructure(dirStructure[property])
+            const sortedVal = sortDirStructure(dirStructure[property], sorter)
     
             sortedDirStructure[property] = sortedVal
         }
